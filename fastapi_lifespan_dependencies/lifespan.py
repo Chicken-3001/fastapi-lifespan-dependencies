7c2fd4d258a78d7c8277d7ca4825f20d0f90f6d8
Ruvi Kaplan <thepokoranan@gmail.com>
2024-03-29 03:43:39 +0300

    feat: Added resolution for both normal and lifespan dependencies as parameters to lifespan dependencies.

--- fastapi_lifespan_dependencies/lifespan.py
+++ fastapi_lifespan_dependencies/lifespan.py
@@ -1,11 +1,11 @@
 from typing import Any, AsyncIterator, Callable, Mapping, TypeVar
-from contextlib import AbstractAsyncContextManager, AsyncExitStack, asynccontextmanager
+from contextlib import AsyncExitStack, asynccontextmanager, AbstractAsyncContextManager
 
 from fastapi import FastAPI, Request
+from fastapi.requests import HTTPConnection
+from fastapi.dependencies.utils import solve_dependencies, get_dependant
 
-
-LifespanDependency = Callable[[], AbstractAsyncContextManager[Any]]
-# TODO: Allow dependencies to specify parameters in order to depend on each other
+LifespanDependency = Callable[..., AbstractAsyncContextManager[Any]]
 R = TypeVar("R")
 
 
@@ -19,18 +19,37 @@ class Lifespan:
 
         async with AsyncExitStack() as stack:
             for name, dependency in self.dependencies.items():
-                state[name] = await stack.enter_async_context(dependency())
+                dependant = get_dependant(path="", call=dependency)
+                solved_values, *_ = await solve_dependencies(
+                    request=Request(
+                        scope={
+                            # TODO: This might not work with websockets
+                            "type": "http",
+                            "query_string": "",
+                            "headers": "",
+                            "state": state,
+                        }
+                    ),
+                    dependant=dependant,
+                    async_exit_stack=stack,
+                )
+
+                state[name] = await stack.enter_async_context(
+                    dependency(**solved_values)
+                )
 
             yield state
 
     def register(
-        self, dependency: Callable[[], AsyncIterator[R]]
-    ) -> Callable[[Request], R]:
+        self, dependency: Callable[..., AsyncIterator[R]]
+    ) -> Callable[[HTTPConnection], R]:
+        # TODO: Add synchronous dependency support
         self.dependencies[dependency.__name__] = asynccontextmanager(dependency)
 
         # TODO: figure out why @wraps(dependency) breaks `Depends()` usage
-        def wrapped_dependency(request: Request) -> Any:
-            # TODO: Class based dependencies don't have __name__ by default, so they'll probably break
-            return request.state._state[dependency.__name__]
+        def path_dependency(connection: HTTPConnection) -> Any:
+            # TODO: Class based dependencies don't have __name__ by default,
+            # so they'll probably break
+            return connection.state._state[dependency.__name__]
 
-        return wrapped_dependency
+        return path_dependency
