e18a6565ee0bc94e8c4963d1e0c66ee93a3a8f23
Ruvi Kaplan <thepokoranan@gmail.com>
2024-06-07 02:01:33 +0300

    fix: Added proper error handling for unsolvable dependencies.

--- fastapi_lifespan_dependencies/lifespan.py
+++ fastapi_lifespan_dependencies/lifespan.py
@@ -1,12 +1,12 @@
-from typing import Any, AsyncIterator, Callable, Mapping, TypeVar
-from contextlib import AsyncExitStack, asynccontextmanager, AbstractAsyncContextManager
+from contextlib import AbstractAsyncContextManager, AsyncExitStack, asynccontextmanager
+from typing import Any, AsyncIterator, Callable, Mapping
 
-from fastapi import FastAPI, Request
-from fastapi.requests import HTTPConnection
-from fastapi.dependencies.utils import solve_dependencies, get_dependant
+from fastapi import FastAPI
+from fastapi.dependencies.utils import get_dependant, solve_dependencies
+from fastapi.exceptions import ValidationException
+from starlette.requests import HTTPConnection, Request
 
 LifespanDependency = Callable[..., AbstractAsyncContextManager[Any]]
-R = TypeVar("R")
 
 
 class Lifespan:
@@ -20,19 +20,26 @@ class Lifespan:
         async with AsyncExitStack() as stack:
             for name, dependency in self.dependencies.items():
                 dependant = get_dependant(path="", call=dependency)
-                solved_values, *_ = await solve_dependencies(
-                    request=Request(
-                        scope={
-                            # TODO: This might not work with websockets
-                            "type": "http",
-                            "query_string": "",
-                            "headers": "",
-                            "state": state,
-                        }
-                    ),
+                initial_state_request = Request(
+                    scope={
+                        "type": "http",
+                        "query_string": "",
+                        "headers": "",
+                        "state": state,
+                    }
+                )
+
+                # FIXME: the `background_tasks`, `response`, `dependency_cache`
+                # return values are currently ignored.
+                solved_values, errors, *_ = await solve_dependencies(
+                    request=initial_state_request,
                     dependant=dependant,
                     async_exit_stack=stack,
                 )
+
+                if len(errors) > 0:
+                    raise ValidationException(errors)
+                    # TODO: Normalize errors (see fastapi.routing:313)
 
                 state[name] = await stack.enter_async_context(
                     dependency(**solved_values)
@@ -40,16 +47,15 @@ class Lifespan:
 
             yield state
 
-    def register(
+    def register[R](
         self, dependency: Callable[..., AsyncIterator[R]]
     ) -> Callable[[HTTPConnection], R]:
-        # TODO: Add synchronous dependency support
         self.dependencies[dependency.__name__] = asynccontextmanager(dependency)
-
         # TODO: figure out why @wraps(dependency) breaks `Depends()` usage
+
         def path_dependency(connection: HTTPConnection) -> Any:
             # TODO: Class based dependencies don't have __name__ by default,
             # so they'll probably break
-            return connection.state._state[dependency.__name__]
+            return getattr(connection.state, dependency.__name__)
 
         return path_dependency
